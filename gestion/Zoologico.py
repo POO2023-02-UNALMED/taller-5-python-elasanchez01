3522cb711850861cb925b0256d7d755ed9de3e99
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-24 13:18:48 -0500

    Create Zoologico.py

---
+++ gestion/Zoologico.py
@@ -0,0 +1,13 @@
+class Zoologico():
+
+    def __init__(self, nombre, ubicacion):
+        self._nombre = nombre
+        self._ubicacion = ubicacion
+        self._zonas = []
+
+    def agregarzonas(self, zona):
+        self._zonas.append.zona
+
+    def cantidadTotalAnimales():
+        pass        #Este tiene que ver con el método agregarAnimal en la clase Zona
+    
