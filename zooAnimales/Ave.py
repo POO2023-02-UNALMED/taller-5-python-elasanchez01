99beda58ee1df90be11ae2a6fed18b43a6c73a8f
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-24 13:22:29 -0500

    Create Ave.py

---
+++ zooAnimales/Ave.py
@@ -0,0 +1,21 @@
+from Animal import Animal
+
+class Ave(Animal):
+
+    def __init__(self):
+        self._listado = []
+        self.halcones
+        self.aguilas
+        self._colorPlumas
+
+    def cantidadAves(self):
+        pass
+
+    def movimiento(self):
+        pass
+
+    def crarAlcohon(self):
+        pass
+
+    def crearAguila(self):
+        pass
