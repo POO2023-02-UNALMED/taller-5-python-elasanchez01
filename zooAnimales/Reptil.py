cbe2051fb324adedf20cd87f0a39068ac108a9af
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-24 13:24:47 -0500

    Create Reptil.py

---
+++ zooAnimales/Reptil.py
@@ -0,0 +1,24 @@
+from Animal import Animal
+
+class Reptil(Animal):
+
+    def __init__(self):
+        self.listado = []
+        self.iguanas
+        self.serpientes
+        self._colorEscamas
+        self._largoCola
+
+    def cantidadReptiles(self):
+        pass
+
+    def movimiento(self):
+        pass
+
+    def crearIguana(self):
+        pass
+
+    def crearSerpiente(self):
+        pass
+
+    
