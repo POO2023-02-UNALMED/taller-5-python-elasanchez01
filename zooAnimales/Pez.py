d7fbb7f1dd24d646a90104584fb14d2a3ba5d803
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-24 13:24:03 -0500

    Create Pez.py

---
+++ zooAnimales/Pez.py
@@ -0,0 +1,22 @@
+from Animal import Animal
+
+class Pez(Animal):
+
+    def __init__(self):
+        self.listado = []
+        self.salmones
+        self.balacaos
+        self._colorEscamas
+        self._cantidadAletas
+
+    def cantidadPeces(self):
+        pass
+
+    def movimiento(self):
+        pass
+
+    def crearIguana(self):
+        pass
+
+    def crearSerpiente(self):
+        pass
