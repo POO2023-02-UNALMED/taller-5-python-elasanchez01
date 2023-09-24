eb60de5056f5eacc6c75cd2e9fdc4b4c293173d4
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-24 13:23:18 -0500

    Create Mamifero.py

---
+++ zooAnimales/Mamifero.py
@@ -0,0 +1,16 @@
+class Mamifero(Animal):
+
+    def __init__(self):
+        self.caballos
+        self.leones
+        self._pelaje
+        self._patas
+
+    def cantidadMamiferos(self):
+        pass
+
+    def crearCaballo(self):
+        pass
+
+    def crearLeon(self):
+        pass
