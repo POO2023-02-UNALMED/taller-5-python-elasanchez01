d6bff31bbb8d118b42fc4b329a56b5219f533aa6
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-24 13:20:46 -0500

    Create Anfibio.py

---
+++ zooAnimales/Anfibio.py
@@ -0,0 +1,22 @@
+from Animal import Animal
+
+class Anfibio(Animal):
+
+    def __init__(self):
+        self._listado = []
+        self.ranas
+        self.salamandras
+        self._colorPiel
+        self._venenoso
+
+    def cantidadAnfibios(self):
+        pass
+
+    def movimiento(self):
+        pass
+
+    def crarRana(self):
+        pass
+
+    def crearSalamandra(self):
+        pass
