c3be7dc6ae38d766fad7588b4dbb6207f7efa3c9
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 11:25:55 -0500

    Update reptil.py

--- zooAnimales/reptil.py
+++ zooAnimales/reptil.py
@@ -3,8 +3,8 @@ from zooAnimales.animal import Animal
 class Reptil(Animal):
 
    _listado = []
-    iguanas = 0
-    serpientes = 0
+   iguanas = 0
+   serpientes = 0
     
     def __init__(self, nombre, edad, habitat, genero, colorEscamas, largoCola):
         super().__init__(nombre, edad, habitat, genero)
