d4a2cfb592b1ecb164f86947851fbd13b4fa8c3b
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 10:55:31 -0500

    Update zona.py

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,17 +1,25 @@
-from gestion.zoologico import Zoologico
-from zooanimales.animal import Animal
+class Zona():
 
-class Zona:
-
-    def __init__(self, nombre, zoo):
+    def __init__(self, nombre, zoo = None):
         self._nombre = nombre
         self._zoo = zoo
         self._animales = []
 
     def agregarAnimales(self, animal):
-        self._animales.append.animal
+        self._animales.append(animal)
 
-    def cantidadAnimales(self):
-        pass                       #no se como hacerlooooo
+   def cantidadAnimales(self):
+        return len(self._animales)
     
+    def getNombre(self):
+        return self._nombre
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getZoo(self):
+        return self._zoo
+    
+    def setZoo(self, zoo):
+        self._zoo = zoo
     
