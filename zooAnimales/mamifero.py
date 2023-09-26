1da59cc7042b22ad14d7fb225fd965b7762f054f
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 11:06:38 -0500

    Update mamifero.py

--- zooAnimales/mamifero.py
+++ zooAnimales/mamifero.py
@@ -1,16 +1,37 @@
+from zooAnimales.animal import Animal
 class Mamifero(Animal):
+    _listado = []
+    caballos = 0
+    leones = 0
 
-    def __init__(self):
-        self.caballos
-        self.leones
-        self._pelaje
-        self._patas
+    def __init__(self, nombre, edad, habitat, genero, pelaje, patas):
+        super().__init__(nombre, edad, habitat, genero)
+        self._pelaje = pelaje
+        self._patas = patas
+        Mamifero._listado.append(self)
 
-    def cantidadMamiferos(self):
-        pass
+    @staticmethod
+    def cantidadMamiferos():
+        return len(Mamifero._listado)
+    
+    @classmethod
+    def crearCaballo(cls, nombre, edad, genero):
+        cls.caballos += 1 
+        return cls(nombre, edad, "pradera", genero, True, 4)
 
-    def crearCaballo(self):
-        pass
+    @classmethod
+    def crearLeon(cls, nombre, edad, genero):
+        cls.leones += 1 
+        return cls(nombre, edad, "selva", genero, True, 4)
 
-    def crearLeon(self):
-        pass
+    def isPelaje(self):
+        return self._pelaje
+
+    def setPelaje(self, pelaje):
+        self._pelaje = pelaje
+
+    def getPatas(self):
+        return self._patas
+    
+    def setPatas(self, patas):
+        self._patas = patas
