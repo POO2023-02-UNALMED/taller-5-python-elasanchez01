f4b47c7951124ef9b56b66e58b5d47b4de2bc799
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 11:00:26 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,20 +1,66 @@
-from gestion.zona import Zona
+import zooAnimales
 
 class Animal():
     _totalAnimales = 0
+    _zona = ""
 
     def __init__(self, nombre, edad, habitad, genero):
         self._nombre = nombre
         self._edad = edad
         self._habitad = habitad
         self._genero = genero
-        self._zona = []
+        Animal._totalAnimales += 1
 
-    def movimiento(self):
-        pass
+    def movimiento():
+        return "desplazarse"
 
-    def totalPorTipo(self):
-        pass
-
+    @staticmethod
+    def totalPorTipo():
+        return "Mamiferos : " + str(zooAnimales.mamifero.Mamifero.cantidadMamiferos()) + "\nAves : " + str(zooAnimales.ave.Ave.cantidadAves()) + "\nReptiles : " + str(zooAnimales.reptil.Reptil.cantidadReptiles()) + "\nPeces : " + str(zooAnimales.pez.Pez.cantidadPeces()) + "\nAnfibios : " + str(zooAnimales.anfibio.Anfibio.cantidadAnfibios())
+    
     def toString(self):
-        pass
+        if self._zona != "":
+            return "Mi nombre es " + self._nombre + ", tengo una edad de " + str(self._edad) + ", habito en " + self._habitat + " y mi genero es " + self._genero + ", la zona en la que me ubico es " + self._zona.getNombre() + ", en el zoo " + self._zona.getZoo().getNombre()
+        else:
+            return "Mi nombre es " + self._nombre + ", tengo una edad de " + str(self._edad) + ", habito en " + self._habitat + " y mi genero es " + self._genero
+    
+    @classmethod
+    def getTotalAnimales(cls):
+        return cls._totalAnimales
+
+    @classmethod
+    def setTotalAnimales(cls, totalAnimales):
+        cls._totalAnimales = totalAnimales
+
+    @classmethod
+    def getZona(cls):
+        return cls._zona
+    
+    @classmethod
+    def setZona(cls, zona):
+        cls._zona = zona
+
+    def getNombre(self):
+        return self._nombre
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getEdad(self):
+        return self._edad
+    
+    def setEdad(self, edad):
+        self._edad = edad
+    
+    def getHabitat(self):
+        return self._habitat
+    
+    def setEdad(self, habitat):
+        self._habitat = habitat
+    
+    def getGenero(self):
+        return self._genero
+    
+    def setGenero(self, genero):
+        self._genero = genero
+    
