0b6be20e16cc25a20c397fd03bcd5c6a99f3a715
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 11:05:16 -0500

    Update ave.py

--- zooAnimales/ave.py
+++ zooAnimales/ave.py
@@ -1,21 +1,35 @@
-from Animal import Animal
+from zooAnimales.animal import Animal
 
 class Ave(Animal):
+    _listado = []
+    halcones = 0
+    aquilas = 0
 
-    def __init__(self):
-        self._listado = []
-        self.halcones
-        self.aguilas
-        self._colorPlumas
+     def __init__(self, nombre, edad, habitat, genero, colorPlumas):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorPlumas = colorPlumas
+        Ave._listado.append(self)
 
-    def cantidadAves(self):
-        pass
+    @staticmethod
+    def cantidadAves():
+        return len(Ave._listado)
 
-    def movimiento(self):
-        pass
+    @staticmethod
+    def movimiento():
+        return "volar"
+    
+    @classmethod
+    def crearHalcon(cls, nombre, edad, genero):
+        cls.halcones += 1 
+        return cls(nombre, edad, "montanas", genero, "cafe glorioso")
 
-    def crarAlcohon(self):
-        pass
+    @classmethod
+    def crearAguila(cls, nombre, edad, genero):
+        cls.aguilas += 1 
+        return cls(nombre, edad, "montanas", genero, "blanco y amarillo")
 
-    def crearAguila(self):
-        pass
+    def getColorPlumas(self):
+        return self._colorPlumas
+
+    def setColorPlumas(self, colorPlumas):
+        self._colorPlumas = colorPlumas
