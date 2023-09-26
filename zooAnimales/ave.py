2c37d293cece4f8ada310383e7c48bf72edf41c6
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 11:36:14 -0500

    Update ave.py

--- zooAnimales/ave.py
+++ zooAnimales/ave.py
@@ -3,7 +3,7 @@ from zooAnimales.animal import Animal
 class Ave(Animal):
     _listado = []
     halcones = 0
-    aquilas = 0
+    aguilas = 0
 
     def __init__(self, nombre, edad, habitat, genero, colorPlumas):
         super().__init__(nombre, edad, habitat, genero)
